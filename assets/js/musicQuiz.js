6e69dac7051b0978db64e9168a36c035736667d9
Miriam McCay <mii.santos@yahoo.com.br>
2024-04-13 13:33:02 +0100

    add wrapped functionality to display the final message to the user when finish the questions

---
+++ assets/js/musicQuiz.js
@@ -0,0 +1,77 @@
+export const musicQuiz = [
+    {
+        question: "Which artist released the album 'Thriller' in 1982?",
+        options: ["Madonna", "Elton John", "Michael Jackson", "David Bowie"],
+        correctAnswer: "Michael Jackson",
+        image:"./assets/images/michael.jpg"
+    },
+    
+    {
+        question: "Which band performed the hit song 'Bohemian Rhapsody'?",
+        options: ["The Beatles", "Queen", "Led Zeppelin", "The Rolling Stones"],
+        correctAnswer: "Queen",
+        image:"./assets/images/queen.jpg"
+    },
+    {
+        question: "What is the name of Beyoncé's fanbase?",
+        options: ["BeyHive", "Queen Bees", "BeyArmy", "BeyFanatics"],
+        correctAnswer: "BeyHive",
+        image: "./assets/images/beyonce.webp"
+    },
+    
+    
+    {
+        question: "Who is the lead vocalist of the band Coldplay?",
+        options: ["Chris Martin", "Thom Yorke", "Eddie Vedder", "Brandon Flowers"],
+        correctAnswer: "Chris Martin",
+        image:  "./assets/images/chris-martin.webp"
+    },
+    {
+        question: "Who is the lead vocalist and founder of the Foo Fighters?",
+        options: ["Dave Grohl", "Kurt Cobain", "Chris Cornell", "Eddie Vedder"],
+        correctAnswer: "Dave Grohl",
+        image:"./assets/images/foo-fighters-dave-grohl.webp"
+    },
+    {
+        question: "Which rock band is famous for their album 'Back in Black'?",
+        options: ["AC/DC", "Guns N' Roses", "Metallica", "Nirvana"],
+        correctAnswer: "AC/DC",
+        image: "./assets/images/acdc.webp"
+    },
+    {
+        question: "Who is known as the 'Queen of Pop'?",
+        options: ["Madonna", "Beyoncé", "Taylor Swift", "Adele"],
+        correctAnswer: "Madonna",
+        image: "./assets/images/madonna.jpg"
+    },
+    {
+        question: "Which iconic guitarist was known for playing with his teeth and behind his head?",
+        options: ["Jimi Hendrix", "Eric Clapton", "Jimmy Page", "Stevie Ray Vaughan"],
+        correctAnswer: "Jimi Hendrix",
+        image: "./assets/images/jimi-hendrix.webp"
+    },
+    {
+        question: "Which British band released the album 'The Dark Side of the Moon'?",
+        options: ["Pink Floyd", "The Beatles", "Led Zeppelin", "Queen"],
+        correctAnswer: "Pink Floyd",
+        image: "./assets/images/pinkfloyd.webp"
+    },
+    {
+        question: "Who is the lead singer of the band U2?",
+        options: ["Bono", "Chris Martin", "Thom Yorke", "Eddie Vedder"],
+        correctAnswer: "Bono",
+        image:"./assets/images/bono.webp"
+    },
+    {
+        question: "Which Irish singer-songwriter gained international fame with her hit song 'Nothing Compares 2 U'?",
+        options: ["Enya", "Dolores O'Riordan", "Sinead O'Connor", "Imelda May"],
+        correctAnswer: "Sinead O'Connor",
+        image: "./assets/images/sinead.webp"
+    },
+    {
+        question: "Which artist released the hit single 'Shape of You'?",
+        options: ["Ed Sheeran", "Sam Smith", "Justin Bieber", "Shawn Mendes"],
+        correctAnswer: "Ed Sheeran",
+        image: "./assets/images/edsheeran.webp"
+    }
+  ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+import { musicQuiz } from "./musicQuiz.js";
 let currentQuestionIndex = 0;
+let optionSelected = false; // Variável para controlar se uma opção foi selecionada
+let totalQuestions = musicQuiz.length;
 document.addEventListener("DOMContentLoaded", function() {
     let startQuizButton = document.getElementById('start-quiz');
     let instructionContainer = document.getElementById('instruction-container');
@@ -9,14 +12,10 @@ document.addEventListener("DOMContentLoaded", function() {
     let userName = document.getElementById('username');
     let formUserRegister = document.getElementById('user-register');
     let labelContainer = document.getElementById('label-container'); // Adicionar a referência ao elemento
-    let optionSelected = false; // Variável para controlar se uma opção foi selecionada
-
+    let wrapper = document.getElementById('content');
     let userLabel = document.getElementById('user-label'); // Corrigir o ID aqui
-
-
     let nextButton = document.getElementById('next-btn');
     nextButton.disabled = true; // Desabilitar o botão "Next" inicialmente
-
     nextButton.addEventListener('click', function() {
         console.log('Next button clicked');
         currentQuestionIndex++;
@@ -27,7 +26,26 @@ document.addEventListener("DOMContentLoaded", function() {
             showQuestion(musicQuiz[currentQuestionIndex]);
             nextButton.disabled = true; // Desabilitar o botão "Next" após avançar
         } else {
-            alert('Quiz completed!');
+            let scoreFeedback = "Oh no! It seems like you struggled a bit. Don't worry, there's always room for improvement!";
+            if (score > 4) {
+                scoreFeedback = "Not bad at all! You're on the right track. Keep it up!";
+            }
+            if (score > 6) {
+                scoreFeedback = "Great job! You're doing pretty well. Keep practicing to reach even higher!";
+            }
+            if (score > 9) {
+                scoreFeedback = "Incredible performance! Are you secretly a radio DJ? Keep shining!";
+            }
+            wrapper.innerHTML = `
+                <div class="quiz-result">
+                    <h1>Well done, ${userName.value}, you got to the end of the quiz!</h1>
+                    <p>Your final score: <span class ="flashing"><u>${score}/${totalQuestions}</u></span></p>
+                    <p>${scoreFeedback}</p>
+                </div>
+                <div class="restart-quiz">
+                    <button onclick="window.location.reload()">Play Again</button>
+                </div>
+            `;
         }
     });
     formUserRegister.addEventListener('submit', function(event){
@@ -38,13 +56,10 @@ document.addEventListener("DOMContentLoaded", function() {
             startQuiz(); // Iniciar o quiz
         }
     });
-
-
     // Adicione o evento de clique para o botão de reinício
     restartQuizButton.addEventListener('click', function() {
         restartQuiz();
     });
-
     function startQuiz() {
         console.log('Start quiz button clicked');
         startQuizButton.classList.add('hidden');
@@ -57,113 +72,25 @@ document.addEventListener("DOMContentLoaded", function() {
         showQuestion(musicQuiz[currentQuestionIndex]);
         updateScoreDisplay(0);
     }
-
-    // Função para reiniciar o quiz
-    function restartQuiz() {
-        console.log('Restart quiz button clicked');
-        currentQuestionIndex = 0; // Reiniciar o índice da pergunta atual
-        score = 0;
-        clearInterval(intervalId);
-        updateScoreDisplay(); // Reiniciar a exibição da pontuação
-        showQuestion(musicQuiz[currentQuestionIndex]); // Exibir a primeira pergunta novamente
+    /**
+     * landing page user validation
+     * @returns true of false on validation of user
+     */
+    function validateUser() {
+        let reg = /^[A-Za-z]+$/;
+        if (userName.value == "" || !userName.value.match(reg)) {
+            alert("Please type in letters, no empty space or numbers!!!")
+            userName.focus();
+            return false;
+        } else {
+            return true;
+        }
     }
-
-    //landing page user validation
-function validateUser() {
-    let reg = /^[A-Za-z]+$/;
-    if (userName.value == "" || !userName.value.match(reg)) {
-        alert("Please type in letters, no empty space or numbers!!!")
-        userName.focus();
-        return false;
-    } else {
-        return true;
-    }
-}
 });
-
-const musicQuiz = [
-    {
-        question: "Which artist released the album 'Thriller' in 1982?",
-        options: ["Madonna", "Elton John", "Michael Jackson", "David Bowie"],
-        correctAnswer: "Michael Jackson",
-        image:"./assets/images/michael.jpg"
-    },
-    
-    {
-        question: "Which band performed the hit song 'Bohemian Rhapsody'?",
-        options: ["The Beatles", "Queen", "Led Zeppelin", "The Rolling Stones"],
-        correctAnswer: "Queen",
-        image:"./assets/images/queen.jpg"
-    },
-    {
-        question: "What is the name of Beyoncé's fanbase?",
-        options: ["BeyHive", "Queen Bees", "BeyArmy", "BeyFanatics"],
-        correctAnswer: "BeyHive",
-        image: "./assets/images/beyonce.webp"
-    },
-    
-    
-    {
-        question: "Who is the lead vocalist of the band Coldplay?",
-        options: ["Chris Martin", "Thom Yorke", "Eddie Vedder", "Brandon Flowers"],
-        correctAnswer: "Chris Martin",
-        image:  "./assets/images/chris-martin.webp"
-    },
-    {
-        question: "Who is the lead vocalist and founder of the Foo Fighters?",
-        options: ["Dave Grohl", "Kurt Cobain", "Chris Cornell", "Eddie Vedder"],
-        correctAnswer: "Dave Grohl",
-        image:"./assets/images/foo-fighters-dave-grohl.webp"
-    },
-    {
-        question: "Which rock band is famous for their album 'Back in Black'?",
-        options: ["AC/DC", "Guns N' Roses", "Metallica", "Nirvana"],
-        correctAnswer: "AC/DC",
-        image: "./assets/images/acdc.webp"
-    },
-    {
-        question: "Who is known as the 'Queen of Pop'?",
-        options: ["Madonna", "Beyoncé", "Taylor Swift", "Adele"],
-        correctAnswer: "Madonna",
-        image: "./assets/images/madonna.jpg"
-    },
-    {
-        question: "Which iconic guitarist was known for playing with his teeth and behind his head?",
-        options: ["Jimi Hendrix", "Eric Clapton", "Jimmy Page", "Stevie Ray Vaughan"],
-        correctAnswer: "Jimi Hendrix",
-        image: "./assets/images/jimi-hendrix.webp"
-    },
-    {
-        question: "Which British band released the album 'The Dark Side of the Moon'?",
-        options: ["Pink Floyd", "The Beatles", "Led Zeppelin", "Queen"],
-        correctAnswer: "Pink Floyd",
-        image: "./assets/images/pinkfloyd.webp"
-    },
-    {
-        question: "Who is the lead singer of the band U2?",
-        options: ["Bono", "Chris Martin", "Thom Yorke", "Eddie Vedder"],
-        correctAnswer: "Bono",
-        image:"./assets/images/bono.webp"
-    },
-    {
-        question: "Which Irish singer-songwriter gained international fame with her hit song 'Nothing Compares 2 U'?",
-        options: ["Enya", "Dolores O'Riordan", "Sinead O'Connor", "Imelda May"],
-        correctAnswer: "Sinead O'Connor",
-        image: "./assets/images/sinead.webp"
-    },
-    {
-        question: "Which artist released the hit single 'Shape of You'?",
-        options: ["Ed Sheeran", "Sam Smith", "Justin Bieber", "Shawn Mendes"],
-        correctAnswer: "Ed Sheeran",
-        image: "./assets/images/edsheeran.webp"
-    }
-];
-
 function showQuestion(quizItem) {
     let progress = document.getElementById('progress-bar-fill');
     let progressDiv = document.getElementById('time'); //
     let messageFinishTime = document.getElementById('time-finish');
-
     // Ocultar a imagem
     document.getElementById('image-container').classList.add('hidden');
     progressDiv.style.display = 'block';
@@ -171,29 +98,22 @@ function showQuestion(quizItem) {
     // Exibir o question container
     let questionContainer = document.getElementById('question-container');
     questionContainer.classList.remove('hidden');
-
     // Exibir a barra de progresso e resetar para 0%
     progress.style.display = 'block';
     progress.style.width = '0%';
-
     // Ocultar a mensagem de tempo esgotado
     messageFinishTime.classList.add('hidden');
-
     // Exibir a pergunta e as opções
     document.getElementById('question').textContent = quizItem.question;
     showOptions(quizItem.options, quizItem.correctAnswer);
-
     // Iniciar a contagem regressiva
     startCountdown(6, progress, progressDiv, messageFinishTime);
 }
-
 function showOptions(options, correctAnswer) {
     let optionButtons = document.querySelectorAll('.option');
     let feedback = document.getElementById('feedback');
     let nextButton = document.getElementById('next-btn');
-
     optionSelected = false; // Reiniciar a variável de controle de seleção de opção
-
     for (let i = 0; i < options.length; i++) {
         optionButtons[i].textContent = options[i];
         optionButtons[i].disabled = false; // Habilitar todas as opções
@@ -204,7 +124,14 @@ function showOptions(options, correctAnswer) {
             nextButton.disabled = false; // Habilitar o botão "Next" após selecionar uma opção
         });
     }
-
+    // Pausar qualquer áudio em andamento antes de reproduzir um novo áudio
+    let audios = document.querySelectorAll('audio');
+    audios.forEach(audio => {
+        if (!audio.paused) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    });
     // Ocultar o feedback após mostrar as opções
     feedback.classList.add('hidden');
 }
@@ -217,7 +144,6 @@ function checkAnswer(event, correctAnswer, optionButtons, feedback) {
     let questionContainer = document.getElementById('question-container');
     let progressDiv = document.getElementById('time');
     let messageFinishTime = document.getElementById('time-finish');
-
     if (selectedOption === correctAnswer) {
         incrementScore();
         console.log("Score incremented");
@@ -235,28 +161,22 @@ function checkAnswer(event, correctAnswer, optionButtons, feedback) {
         feedbackAudio.src = "incorrect-sound.mp3"; // Definir o arquivo de áudio incorreto
     }
     feedback.classList.remove('hidden'); // Mostrar o feedback após a seleção
-
     // Desativar todos os botões após a seleção
     optionButtons.forEach(button => {
         button.disabled = true;
     });
-
     // Reproduzir o áudio de feedback
+    feedbackAudio.volume = 0.5;// for a better userExperience
     feedbackAudio.play();
-
     // Ocultar a barra de progresso e resetar para 0%
     let progress = document.getElementById('progress-bar-fill');
     progress.style.display = 'none';
     progress.style.width = '0%';
-
     // Ocultar o contador de tempo
     progressDiv.style.display = 'none';
-
     // Ocultar a mensagem de tempo esgotado
     messageFinishTime.classList.add('hidden');
 }
-
-
 function showImage(quizItem) {
     let imageContainer = document.getElementById('image-container');
     let artistImage = document.getElementById('artist-image');
@@ -267,35 +187,25 @@ function showImage(quizItem) {
         imageContainer.classList.add('hidden'); // Ocultar container se não houver imagem
     }
 }
-
-
 let score = 0; // Declara a variável score aqui
-
 function incrementScore() {
     score++;
 }
-
 function updateScoreDisplay() {
-    let totalQuestions = musicQuiz.length;
+    
     document.getElementById("score").innerText = `${score}/${totalQuestions}`;
 }
-
 let intervalId; // Variável para armazenar o intervalo do countdown
-
 function startCountdown(durationSeconds, progress, progressDiv, messageFinishTime) {
     let totalTime = durationSeconds * 1000;
     let intervalMs = 500;
     let currentTime = 0;
     let audioPlayed = false; // Variável para controlar se o áudio já foi reproduzido
-
     progress.style.width = '0%';
-
     let optionButtons = document.querySelectorAll('.option');
     let clockAudio = document.getElementById('clock-audio');
-
     // Ajustar a duração do áudio com base na duração total da contagem
     let audioDuration = totalTime < 11000 ? totalTime : 11000; // Limite de 11 segundos para o áudio
-
     intervalId = setInterval(function() {
         currentTime += intervalMs;
         if (currentTime <= totalTime) {
